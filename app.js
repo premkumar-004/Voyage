6797ffb0fd4d9391b54a0c0b41c035680dfffa2c
premkumar-004 <premkumarkardale@gmail.com>
2024-09-10 18:29:46 +0530

    created listings of Apartment and hotels schema in the database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/voyage';
+const Listing = require("./models/listing.js");
 
 app.get("/", (req, res) => {
     res.send("Hello");
@@ -16,7 +17,18 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My new villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Hyderabad",
+        country: "India",
+    });
+    await sampleListing.save();
+    console.log("Sample was Saved");
+    res.send("Successful");
+})
 
 app.listen(8080, () => {
     console.log("Listening on port no. 8080");
