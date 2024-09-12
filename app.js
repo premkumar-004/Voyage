9071e1e50b704cd51185435311019d1d7ffcc079
premkumar-004 <premkumarkardale@gmail.com>
2024-09-13 00:31:55 +0530

    created index route and a index.ejs to view all the listings

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/voyage';
 const Listing = require("./models/listing.js");
+const path = require("path");
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 main().then((res) => {
     console.log("Connected to DB");
@@ -17,17 +17,23 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My new villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Hyderabad",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("Sample was Saved");
-    res.send("Successful");
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Hyderabad",
+//         country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was Saved");
+//     res.send("Successful");
+// })
+
+//Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
 })
 
 app.listen(8080, () => {
