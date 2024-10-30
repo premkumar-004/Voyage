afa130f6e0501ce9ddc1fbf8ce01ac3006537771
premkumar-004 <premkumarkardale@gmail.com>
2024-10-30 18:54:22 +0530

    created error response page

--- app.js
+++ app.js
@@ -86,7 +86,8 @@ app.all("*", (req, res, next) => {
 //Middleware 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Page Not Found" } = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { message });
+    // res.status(statusCode).send(message);
 })
 
 app.listen(8080, () => {
