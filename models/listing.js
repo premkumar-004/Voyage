54858f1dee29ac05b9ce2cd781ba6431a443ea41
premkumar-004 <premkumarkardale@gmail.com>
2024-11-02 22:36:14 +0530

    added review section to each listing in listing.js schema

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -14,6 +15,12 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
+    review: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
