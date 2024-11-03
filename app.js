59ff560b75126a36981cb70ab813c8d46cc4005d
premkumar-004 <premkumarkardale@gmail.com>
2024-11-03 19:50:43 +0530

    handling deletion for review

--- app.js
+++ app.js
@@ -109,6 +109,14 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => 
     res.redirect(`/listings/${id}`);
 }))
 
+//Review Deletion
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not found"));
 });
