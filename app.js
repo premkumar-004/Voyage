97a085e1f706a4b5fc9976d942a99c9977ba4f45
premkumar-004 <premkumarkardale@gmail.com>
2024-11-03 16:59:03 +0530

    Displaying reviews on it's individual page

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get("/listings/new", (req, res) => {
 //show route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("./listings/show.ejs", { listing });
 }))
 
