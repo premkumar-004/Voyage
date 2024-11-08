d846bfd82465042d224d268b16ccf4066afc68c7
premkumar-004 <premkumarkardale@gmail.com>
2024-11-08 19:24:11 +0530

    login credentials authentication using passport

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -20,5 +21,15 @@ router.post("/signup", async (req, res) => {
         res.redirect("/signup");
     }
 })
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+})
+
+router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }),
+    async (req, res) => {
+        req.flash("success", "Welcome to Voyage");
+        res.redirect("/listings");
+    })
 
 module.exports = router;
