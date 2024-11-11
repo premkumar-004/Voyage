27b0663802310298599781313f5cbacf449127ab
premkumar-004 <premkumarkardale@gmail.com>
2024-11-11 19:54:22 +0530

    authorization for reviews

--- routes/review.js
+++ routes/review.js
@@ -3,13 +3,15 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 //Review / Comments Route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     let newReview = new Review(req.body.review);
+
+    newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
 
