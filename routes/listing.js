9980566b673236aab17d17d9957cbea79c2ac0b6
premkumar-004 <premkumarkardale@gmail.com>
2024-11-15 17:48:09 +0530

    using env file to store secret keys

--- routes/listing.js
+++ routes/listing.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 //Index Route
 //create route
 router.route("/")
     .get(wrapAsync(listingController.index))
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    // .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    .post(upload.single('listing[image]'), (req, res) => {
+        res.send(req.file);
+    });
 
 //New create route
 //Put above /:id other wise /new will be treated as an id and this route will not work
