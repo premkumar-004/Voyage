f5da7849c860f1a18530f737fa1c7aae9ef9e547
premkumar-004 <premkumarkardale@gmail.com>
2024-11-17 00:40:04 +0530

    previewing image for updation

--- controllers/listings.js
+++ controllers/listings.js
@@ -32,7 +32,9 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash("error", "Listing Donot Exists!");
         res.redirect("/listings");
     }
-    res.render("./listings/edit.ejs", { listing });
+    let originalImageUrl = listing.image.url
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+    res.render("./listings/edit.ejs", { listing, originalImageUrl });
 };
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
